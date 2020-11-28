56e8b6cac3d8fe8a5d232f5f4094f3fab99d81b9
wr-rainforest <71073377+wr-rainforest@users.noreply.github.com>
2020-11-28 20:48:13 +0900

    테스트 케이스 작성: PostRequestTests, CommentListRequestTests

---
+++ CSInside.Test/CommentListRequestTests.cs
@@ -0,0 +1,57 @@
+﻿using Xunit;
+
+namespace CSInside.Test
+{
+    public class CommentListRequestTests
+    {
+        static ApiService service;
+        static ApiService Service
+        {
+            get
+            {
+                if (service == null)
+                    service = new ApiService(new AuthTokenProvider());
+                return service;
+            }
+        }
+
+        [Fact]
+        public async void CommentListRequestTest()
+        {
+            CommentListRequest request = Service.CreateCommentListRequest("programming", 1540251);
+            Comment[] comments = await request.ExecuteAsync();
+            Assert.True(comments.Length > 1);
+        }
+
+        [Fact]
+        public async void AdultGalleryCommentListRequestTest()
+        {
+            CommentListRequest request = Service.CreateCommentListRequest("nude", 122229);
+            Comment[] comments = await request.ExecuteAsync();
+            Assert.True(comments.Length > 1);
+        }
+
+        [Fact]
+        public async void RestrictedGalleryCommentListRequestTest()
+        {
+            CommentListRequest request = Service.CreateCommentListRequest("python", 124);
+            Comment[] comments = await request.ExecuteAsync();
+            Assert.True(comments.Length > 1);
+        }
+
+        [Fact]
+        public async void DeletedPostCommentListRequestTest()
+        {
+            CommentListRequest request = Service.CreateCommentListRequest("programming", 1540253);
+            Assert.Null(await request.ExecuteAsync());
+        }
+
+        [Fact]
+        public async void CommentListRequestWithInvalidAuthTokenTest()
+        {
+            CommentListRequest request = new ApiService(new AuthTokenProvider("InvalidToken")).CreateCommentListRequest("programming", 1540253);
+            var exception = await Assert.ThrowsAsync<CSInsideException>(async () => { await request.ExecuteAsync(); });
+            Assert.Contains("올바르지 않은 인증 토큰", exception.Message);
+        }
+    }
+}
---
+++ CSInside.Test/PostRequestTests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Xunit;
+
+namespace CSInside.Test
+{
+    public class PostRequestTests
+    {
+        static ApiService service;
+        static ApiService Service 
+        {
+            get
+            {
+                if(service == null)
+                    service = new ApiService(new AuthTokenProvider());
+                return service;
+            }
+        }
+
+        [Fact]
+        public async void PostRequestTest()
+        {
+            PostRequest request = Service.CreatePostRequest("programming", 1540251);
+            Post post = await request.ExecuteAsync();
+            Assert.True(post != null);
+            Assert.True(post.Title == "Test 1");
+            Assert.True(post.TimeStamp == new DateTime(2020, 11, 28, 11, 45, 00));
+        }
+
+        [Fact]
+        public async void DeletedPostRequestTest()
+        {
+            PostRequest request = Service.CreatePostRequest("programming", 1540253);
+            Post post = await request.ExecuteAsync();
+            Assert.True(post == null);
+        }
+
+        [Fact]
+        public async void PostRequestWithInvalidAuthTokenTest()
+        {
+            PostRequest request = new ApiService(new AuthTokenProvider("InvalidToken")).CreatePostRequest("programming", 1540253);
+            var exception = await Assert.ThrowsAsync<CSInsideException>(async () => { await request.ExecuteAsync(); });
+            Assert.Contains("올바르지 않은 인증 토큰", exception.Message);
+        }
+
+        [Fact]
+        public async void AdultGalleryPostRequestTest()
+        {
+            PostRequest request = Service.CreatePostRequest("nude", 1540253);
+            var exception = await Assert.ThrowsAsync<CSInsideException>(async () => { await request.ExecuteAsync(); });
+            Assert.Contains("성인 갤러리", exception.Message);
+        }
+
+        [Fact]
+        public async void RestrictedGalleryPostRequestTest()
+        {
+            PostRequest request = Service.CreatePostRequest("python", 124);
+            var exception = await Assert.ThrowsAsync<CSInsideException>(async () => { await request.ExecuteAsync(); });
+            Assert.Contains("접근이 제한된", exception.Message);
+        }
+    }
+}
